2053cfb43cab04ff348dd365c21965389c873d05
JacobBoxCEO <abarter082@gmail.com>
2024-07-04 22:07:05 -0400

    added bad to the bone block (wip)

---
+++ src/main/java/net/jacobBoxCeo/firstModYippee/blocks/BadToTheBoneBlock.java
@@ -0,0 +1,29 @@
+package net.jacobBoxCeo.firstModYippee.blocks;
+
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class BadToTheBoneBlock extends Block
+{
+    public BadToTheBoneBlock(Properties pProperties)
+    {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult use(
+            BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit)
+    {
+        pLevel.playSound(pPlayer,pPos, SoundEvents.GENERIC_EXPLODE.get(), SoundSource.BLOCKS);
+        return InteractionResult.SUCCESS;
+    }
+}
