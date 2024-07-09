ecd0ad514d73f26d92b9a96aad26d5fdd689abb2
JacobBoxCEO <abarter082@gmail.com>
2024-07-09 02:59:18 -0400

    reworked packingRecipe method

--- src/main/java/net/jacobBoxCeo/gexium/datagen/ModRecipesGen.java
+++ src/main/java/net/jacobBoxCeo/gexium/datagen/ModRecipesGen.java
@@ -5,7 +5,6 @@ import net.jacobBoxCeo.gexium.items.ModItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import net.minecraftforge.registries.RegistryObject;
@@ -23,21 +22,39 @@ public class ModRecipesGen extends RecipeProvider implements IConditionBuilder
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> consumer)
     {
-        threeByThreePackingRecipe(RecipeCategory.MISC, ModBlocks.GEXITE_BLOCK, ModItems.GEXITE, consumer);
-        threeByThreePackingRecipe(RecipeCategory.MISC, ModBlocks.GEXIUM_BLOCK, ModItems.GEXIUM, consumer);
+        packingRecipe(false, RecipeCategory.MISC, ModBlocks.GEXITE_BLOCK, ModItems.GEXITE, consumer);
+        packingRecipe(false, RecipeCategory.MISC, ModBlocks.GEXIUM_BLOCK, ModItems.GEXIUM, consumer);
     }
-    static private void threeByThreePackingRecipe(RecipeCategory category, RegistryObject<Block> block, RegistryObject<Item> item, Consumer consumer)
+    static private void packingRecipe(Boolean isTwoByTwo, RecipeCategory category, RegistryObject<Block> block, RegistryObject<Item> item, Consumer consumer)
     {
-        ShapedRecipeBuilder.shaped(category, block.get())
-                .pattern("###")
-                .pattern("###")
-                .pattern("###")
-                .define('#', item.get())
-                .unlockedBy(getHasName(item.get()), has(item.get()))
-                .save(consumer);
-        ShapelessRecipeBuilder.shapeless(category, item.get(), 9)
-                .requires(block.get())
-                .unlockedBy(getHasName(block.get()), has(block.get()))
-                .save(consumer);
+        if(isTwoByTwo == false)
+        {
+            ShapedRecipeBuilder.shaped(category, block.get())
+                    .pattern("###")
+                    .pattern("###")
+                    .pattern("###")
+                    .define('#', item.get())
+                    .unlockedBy(getHasName(item.get()), has(item.get()))
+                    .save(consumer);
+            ShapelessRecipeBuilder.shapeless(category, item.get(), 9)
+                    .requires(block.get())
+                    .unlockedBy(getHasName(block.get()), has(block.get()))
+                    .save(consumer);
+        }
+        else
+        {
+            ShapedRecipeBuilder.shaped(category, block.get())
+                    .pattern("## ")
+                    .pattern("## ")
+                    .pattern("## ")
+                    .define('#', item.get())
+                    .unlockedBy(getHasName(item.get()), has(item.get()))
+                    .save(consumer);
+            ShapelessRecipeBuilder.shapeless(category, item.get(), 4)
+                    .requires(block.get())
+                    .unlockedBy(getHasName(block.get()), has(block.get()))
+                    .save(consumer);
+
+        }
     }
 }
