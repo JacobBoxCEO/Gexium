bf805363b182878f2be19ff775bebf4af96d4fbf
JacobBoxCEO <abarter082@gmail.com>
2024-07-08 20:17:23 -0400

    implemented for loop to creative tab to automate adding items

--- src/main/java/net/jacobBoxCeo/gexium/creative_mode_tabs/ModCreativeModeTabs.java
+++ src/main/java/net/jacobBoxCeo/gexium/creative_mode_tabs/ModCreativeModeTabs.java
@@ -6,7 +6,9 @@ import net.jacobBoxCeo.gexium.items.ModItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -19,12 +21,14 @@ public class ModCreativeModeTabs
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.GEXITE.get()))
                     .title(Component.translatable("creativetab.silly_mod_tab"))
                     .displayItems((itemDisplayParameters, output) -> {
-                        output.accept(ModItems.GEXITE.get());
-                        output.accept(ModBlocks.GEXITE_BLOCK.get());
-                        output.accept(ModItems.GEXIUM.get());
-                        output.accept(ModBlocks.GEXIUM_BLOCK.get());
-                        output.accept(ModItems.SILLIUM.get());
-                        output.accept(ModBlocks.BAD_TO_THE_BLOCK.get());
+                        for(RegistryObject<Item> item : ModItems.ITEMS.getEntries())
+                        {
+                            output.accept(item.get());
+                        }
+                        for(RegistryObject<Block> block : ModBlocks.BLOCKS.getEntries())
+                        {
+                            output.accept(block.get());
+                        }
                     })
                     .build());
     public static void register(IEventBus eventBus)
