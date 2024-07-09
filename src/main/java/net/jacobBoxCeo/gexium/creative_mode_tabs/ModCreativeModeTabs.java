85e771c2bee993c350d56b82b35bc525e9dafc70
JacobBoxCEO <abarter082@gmail.com>
2024-07-09 17:41:41 -0400

    removed more redundancies from crative mode tabs

--- src/main/java/net/jacobBoxCeo/gexium/creative_mode_tabs/ModCreativeModeTabs.java
+++ src/main/java/net/jacobBoxCeo/gexium/creative_mode_tabs/ModCreativeModeTabs.java
@@ -1,14 +1,12 @@
 package net.jacobBoxCeo.gexium.creative_mode_tabs;
 
 import net.jacobBoxCeo.gexium.Gexium;
-import net.jacobBoxCeo.gexium.blocks.ModBlocks;
 import net.jacobBoxCeo.gexium.items.ModItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
