9cd7ae3cc4add5f7092c1c6d3d018eb2d5776158
JacobBoxCEO <abarter082@gmail.com>
2024-08-02 20:12:54 -0400

    made gexium armor trimmable

--- src/main/java/net/jacobBoxCeo/gexium/datagen/DataGenerators.java
+++ src/main/java/net/jacobBoxCeo/gexium/datagen/DataGenerators.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 
 import java.util.concurrent.CompletableFuture;
 
-@Mod.EventBusSubscriber(modid = Gexium.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = Gexium.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
--- src/main/java/net/jacobBoxCeo/gexium/datagen/ModItemTagGen.java
+++ src/main/java/net/jacobBoxCeo/gexium/datagen/ModItemTagGen.java
@@ -1,9 +1,11 @@
 package net.jacobBoxCeo.gexium.datagen;
 
 import net.jacobBoxCeo.gexium.Gexium;
+import net.jacobBoxCeo.gexium.init.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -14,11 +16,15 @@ public class ModItemTagGen extends ItemTagsProvider {
 
     public ModItemTagGen(PackOutput p_275343_, CompletableFuture<HolderLookup.Provider> p_275729_,
                          CompletableFuture<TagLookup<Block>> p_275322_, @Nullable ExistingFileHelper existingFileHelper) {
-        super(p_275343_, p_275729_, p_275322_, Gexium.MODID, existingFileHelper);
+        super(p_275343_, p_275729_, p_275322_, Gexium.MOD_ID, existingFileHelper);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-
+        this.tag(ItemTags.TRIMMABLE_ARMOR)
+                .add(ModItems.GEXIUM_HELMET.get(),
+                        ModItems.GEXIUM_CHESTPLATE.get(),
+                        ModItems.GEXIUM_LEGGINGS.get(),
+                        ModItems.GEXIUM_BOOTS.get());
     }
 }
