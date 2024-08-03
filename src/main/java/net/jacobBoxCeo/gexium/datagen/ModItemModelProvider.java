413dd9acbc0b00ef515f5ed66fbaa27ffdd38446
JacobBoxCEO <abarter082@gmail.com>
2024-08-02 20:14:21 -0400

    added methods for trimmable armor item models

--- src/main/java/net/jacobBoxCeo/gexium/datagen/ModItemModelProvider.java
+++ src/main/java/net/jacobBoxCeo/gexium/datagen/ModItemModelProvider.java
@@ -4,20 +4,38 @@ import net.jacobBoxCeo.gexium.Gexium;
 import net.jacobBoxCeo.gexium.init.ModBlocks;
 import net.jacobBoxCeo.gexium.init.ModItems;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.armortrim.TrimMaterial;
+import net.minecraft.world.item.armortrim.TrimMaterials;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.LinkedHashMap;
+
 public class ModItemModelProvider extends ItemModelProvider {
+    private static LinkedHashMap<ResourceKey<TrimMaterial>, Float> trimMaterials = new LinkedHashMap<>();
+    static {
+        trimMaterials.put(TrimMaterials.QUARTZ, 0.1F);
+        trimMaterials.put(TrimMaterials.IRON, 0.2F);
+        trimMaterials.put(TrimMaterials.NETHERITE, 0.3F);
+        trimMaterials.put(TrimMaterials.REDSTONE, 0.4F);
+        trimMaterials.put(TrimMaterials.COPPER, 0.5F);
+        trimMaterials.put(TrimMaterials.GOLD, 0.6F);
+        trimMaterials.put(TrimMaterials.EMERALD, 0.7F);
+        trimMaterials.put(TrimMaterials.DIAMOND, 0.8F);
+        trimMaterials.put(TrimMaterials.LAPIS, 0.9F);
+        trimMaterials.put(TrimMaterials.AMETHYST, 1.0F);
+    }
     public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
-        super(output, Gexium.MODID, existingFileHelper);
+        super(output, Gexium.MOD_ID, existingFileHelper);
     }
 
     private static boolean checkIsBlock(RegistryObject<Item> item) {
@@ -27,20 +45,47 @@ public class ModItemModelProvider extends ItemModelProvider {
     }
 
     private static boolean checkIsTool(RegistryObject<Item> item) {
-        if (item.get().canBeDepleted() && new ItemStack(item.get()).getEquipmentSlot() == null) {
-            return true;
-        }
+        if (item.get().canBeDepleted() && item.get().getClass() != ArmorItem.class) return true;
         return false;
+    }
+
+    private static int checkType(RegistryObject<Item> item) {
+        if (item.get().canBeDepleted())
+        {
+            if (item.get() instanceof ArmorItem)
+            {
+                if (checkIsTrimmable(item))
+                {
+                    //trimmed item
+                    return 0;
+                }
+                //simple item
+                return 1;
+            }
+            //tool item
+            return 2;
+        }
+        //simple item
+        return 1;
+    }
+
+    private static boolean checkIsTrimmable(RegistryObject<Item> item) {
+        return true;
     }
 
     @Override
     protected void registerModels() {
         for (RegistryObject<Item> item : ModItems.ITEMS.getEntries()) {
             if (!checkIsBlock(item)) {
-                if (checkIsTool(item))
-                    toolItem(item);
-                else
-                    simpleItem(item);
+                switch (checkType(item))
+                {
+                    case 0:
+                        trimmedArmorItem(item);
+                    case 1:
+                        simpleItem(item);
+                    case 2:
+                        toolItem(item);
+                }
             }
         }
     }
@@ -48,12 +93,60 @@ public class ModItemModelProvider extends ItemModelProvider {
     private ItemModelBuilder toolItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/handheld")).texture("layer0",
-                new ResourceLocation(Gexium.MODID, "item/" + item.getId().getPath()));
+                new ResourceLocation(Gexium.MOD_ID, "item/" + item.getId().getPath()));
     }
 
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(Gexium.MODID, "item/" + item.getId().getPath()));
+                new ResourceLocation(Gexium.MOD_ID, "item/" + item.getId().getPath()));
+    }
+
+    // Shoutout to El_Redstoniano for making this
+    private void trimmedArmorItem(RegistryObject<Item> itemRegistryObject) {
+        final String MOD_ID = Gexium.MOD_ID; // Change this to your mod id
+
+        if(itemRegistryObject.get() instanceof ArmorItem armorItem) {
+            trimMaterials.entrySet().forEach(entry -> {
+
+                ResourceKey<TrimMaterial> trimMaterial = entry.getKey();
+                float trimValue = entry.getValue();
+
+                String armorType = switch (armorItem.getEquipmentSlot()) {
+                    case HEAD -> "helmet";
+                    case CHEST -> "chestplate";
+                    case LEGS -> "leggings";
+                    case FEET -> "boots";
+                    default -> "";
+                };
+
+                String armorItemPath = "item/" + armorItem;
+                String trimPath = "trims/items/" + armorType + "_trim_" + trimMaterial.location().getPath();
+                String currentTrimName = armorItemPath + "_" + trimMaterial.location().getPath() + "_trim";
+                ResourceLocation armorItemResLoc = new ResourceLocation(MOD_ID, armorItemPath);
+                ResourceLocation trimResLoc = new ResourceLocation(trimPath); // minecraft namespace
+                ResourceLocation trimNameResLoc = new ResourceLocation(MOD_ID, currentTrimName);
+
+                // This is used for making the ExistingFileHelper acknowledge that this texture exist, so this will
+                // avoid an IllegalArgumentException
+                existingFileHelper.trackGenerated(trimResLoc, PackType.CLIENT_RESOURCES, ".png", "textures");
+
+                // Trimmed armorItem files
+                getBuilder(currentTrimName)
+                        .parent(new ModelFile.UncheckedModelFile("item/generated"))
+                        .texture("layer0", armorItemResLoc)
+                        .texture("layer1", trimResLoc);
+
+                // Non-trimmed armorItem file (normal variant)
+                this.withExistingParent(itemRegistryObject.getId().getPath(),
+                                mcLoc("item/generated"))
+                        .override()
+                        .model(new ModelFile.UncheckedModelFile(trimNameResLoc))
+                        .predicate(mcLoc("trim_type"), trimValue).end()
+                        .texture("layer0",
+                                new ResourceLocation(MOD_ID,
+                                        "item/" + itemRegistryObject.getId().getPath()));
+            });
+        }
     }
 }
