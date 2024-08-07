9eddb8179e105108638a42887bd14a564cdbdd08
JacobBoxCEO <abarter082@gmail.com>
2024-08-07 04:17:00 -0400

    added utils for adding components for appendHoverText

--- src/main/java/net/jacobBoxCeo/gexium/util/Util.java
+++ src/main/java/net/jacobBoxCeo/gexium/util/Util.java
@@ -1,12 +1,25 @@
 package net.jacobBoxCeo.gexium.util;
 
 import net.jacobBoxCeo.gexium.Gexium;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class Util {
     public static String makeDescriptionId(String pType, @Nullable ResourceLocation pId) {
         return pId == null ? pType + ".unregistered_sadface" : pType + "." + Gexium.MOD_ID + "." + pId.getPath().replace('/', '.');
+    }
+    public static void addLiteralComponent(@NotNull List<Component> pTooltipComponents, String inputString, ChatFormatting color) {
+        pTooltipComponents.add(Component.literal((inputString))
+                .withStyle(color));
+    }
+    public static void addTranslatableComponent(@NotNull List<Component> pTooltipComponents, String type, String location, ChatFormatting color) {
+        pTooltipComponents.add(Component.translatable(makeDescriptionId(type,
+                        new ResourceLocation(location)))
+                .withStyle(color));
     }
 }
