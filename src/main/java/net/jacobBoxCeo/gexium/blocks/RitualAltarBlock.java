8ea34e099cec7cb4dc958d5ac6975070267b90ee
JacobBoxCEO <abarter082@gmail.com>
2024-08-29 04:12:47 -0400

    added overrides for some methods

--- src/main/java/net/jacobBoxCeo/gexium/blocks/RitualAltarBlock.java
+++ src/main/java/net/jacobBoxCeo/gexium/blocks/RitualAltarBlock.java
@@ -1,8 +1,10 @@
 package net.jacobBoxCeo.gexium.blocks;
 
 
+import net.jacobBoxCeo.gexium.blocks.entity.ModBlockEntities;
 import net.jacobBoxCeo.gexium.blocks.entity.RitualAltarBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -13,6 +15,7 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class RitualAltarBlock extends BaseEntityBlock {
@@ -22,23 +25,40 @@ public class RitualAltarBlock extends BaseEntityBlock {
 
     @Override
     public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston) {
-        super.onRemove(pState, pLevel, pPos, pNewState, pMovedByPiston);
+        if(pState.getBlock() != pNewState.getBlock()) {
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if (blockEntity instanceof RitualAltarBlockEntity) {
+                ((RitualAltarBlockEntity)blockEntity).drops();
+            }
+        }
     }
 
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+        if (!pLevel.isClientSide) {
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if(blockEntity instanceof RitualAltarBlockEntity) {
+                NetworkHooks.openScreen((ServerPlayer) pPlayer, (RitualAltarBlockEntity) blockEntity, pPos);
+            } else {
+                throw new IllegalStateException("Mario is missing!");
+            }
+        }
+        return InteractionResult.sidedSuccess(pLevel.isClientSide());
     }
 
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
-        return new RitualAltarBlockEntity(blockPos, blockState)
+        return new RitualAltarBlockEntity(blockPos, blockState);
     }
 
     @Nullable
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
-        return super.getTicker(pLevel, pState, pBlockEntityType);
+        if(pLevel.isClientSide()) {
+            return null;
+        }
+        return createTickerHelper(pBlockEntityType, ModBlockEntities.RITUAL_ALTAR_BE.get(),
+                (level, blockPos, blockState, ritualAltarBlockEntity) -> ritualAltarBlockEntity.tick(level,blockPos, blockState));
     }
 }
